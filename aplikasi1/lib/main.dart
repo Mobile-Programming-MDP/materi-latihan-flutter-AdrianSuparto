6fb5a18dacf23b558dd8e87753269608cf2eca52
Adrians <adriansuparto_2226250027@mhs.mdp.ac.id>
2023-10-16 13:08:05 +0700

    menambahkan icon di app bar
    mencoba belajar flutter dirumah

--- aplikasi1/lib/main.dart
+++ aplikasi1/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: "hello world",
-        theme: ThemeData(primarySwatch: Colors.blue),
+        theme: ThemeData(primarySwatch: Colors.green),
         home: FirstScreen());
   }
 }
